4e46d9eeeabeff770bc20ad972fdfb28644cebc3
yagnesh.patel <yagnesh.patel-e@maf.ae>
2024-01-29 10:09:18 +0530

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # django
+
+python3 -m venv venv
+
+source /venv/bin/activate
---
+++ backend/cfehome/urls.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from django.urls import re_path, include
+
+urlpatterns = [re_path("admin/", admin.site.urls), re_path("api/", include("api.urls"))]
